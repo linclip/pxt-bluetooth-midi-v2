e18775d6932a8d14d6536e65de61840faae6ce88
linclip <linclip@gmail.com>
2025-07-12 22:38:43 +0900

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
-// テストはここに来ます。このパッケージが拡張機能として使用されるときにはコンパイルされません。
+input.onButtonPressed(Button.A, function () {
+    bleMidiTx.noteOn(60, 100, 1) // C4, Velocity 100, Channel 1
+})
+input.onButtonPressed(Button.B, function () {
+    bleMidiTx.noteOff(60, 1) // C4, Channel 1
+})
+bleMidiTx.startBluetoothMidi()
