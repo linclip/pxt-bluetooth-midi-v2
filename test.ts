0d4fcceadf553621e3f2f8d07552bc1579e7dadc
linclip <linclip@gmail.com>
2025-07-13 16:22:44 +0900

    更新 enums.d.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,8 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     bleMidiTx.noteOn(60, 100, 1) // C4, Velocity 100, Channel 1
-    
+    basic.showString("C")
 })
 input.onButtonPressed(Button.B, function () {
     bleMidiTx.noteOff(60, 1) // C4, Channel 1
+    basic.showString("_")
 })
 bleMidiTx.startBluetoothMidi()
